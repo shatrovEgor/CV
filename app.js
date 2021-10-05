e521bf9596c3a278a7e271a1a0338b52f370ca7a
shatrovEgor <shatris43rus@gmail.com>
2021-10-05 23:38:15 +0300

    slider

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const btn = document.querySelector('.mail')
-const screens = document.querySelectorAll('.screen')
+const box = document.querySelector('.box')
 const form = document.querySelector('#form');
 const popup = document.querySelector('.popup');
 const btnCancel = document.querySelector('.btn-cancel')
+
+btn.addEventListener('mouseover', (event) => {
+    console.log('go')
+    btn.classList.add('hover')
+    box.classList.add('hover')
+})
+
+btn.addEventListener('mouseleave', (event) => {
+    btn.classList.remove('hover')
+    box.classList.remove('hover')
+})
 
 btn.addEventListener('click', (event) => {
     form.classList.add('open');
