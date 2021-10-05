2f3d2a20b9c4114b1a9ff98ec5b1a318106696e2
shatrovEgor <shatris43rus@gmail.com>
2021-10-05 12:21:50 +0300

    btn contact me

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const btn = document.querySelector('.mail')
 const screens = document.querySelectorAll('.screen')
+const form = document.querySelector('#form');
+const popup = document.querySelector('.popup');
+const btnCancel = document.querySelector('.btn-cancel')
 
 btn.addEventListener('click', (event) => {
-    console.log('Contact me');
+    form.classList.add('open');
+    popup.classList.add('popup_open');
+})
+
+btnCancel.addEventListener('click', (event) => {
+    form.classList.remove('open');
+    popup.classList.remove('popup_open');
 })
